b139454d821557ce1dfe9b586c4a8ccff8739fbe
Swyrin <dat20036@gmail.com>
2021-10-20 19:23:13 +0700

    Add operator pool

---
+++ pool.py
@@ -0,0 +1,37 @@
+# Operators pool
+# Put this here for editting convenience
+six_stars_pool = ["Aak", "Angelina", "Archetto", "Bappipe", "Blaze",
+                    "Blemishine", "Ceobe", "Ch'en", "Eunectes", "Exusiai",
+                    "Eyjafjalla", "Hellagur", "Hoshiguma", "Ifrit", "Magallan",
+                    "Mostima", "Mountain", "Mudrock", "Nightingale", "Passenger",
+                    "Rosa", "Saga", "Saria", "Schwarz", "Shining",
+                    "Siege", "SilverAsh", "Skadi", "Surtr", "Suzuran",
+                    "Thorns", "Weedy"]
+
+five_stars_pool = ["Andreana", "Aosta", "Asbestos", "Astesia", "Ayerscarpe",
+                    "Beeswax", "Blue Poison", "Broca", "Chiave", "Cliffheart",
+                    "Croissant", "Executor", "FEater", "Firewatch", "Franka",
+                    "Glaucus", "Greythroat", "Hung", "Iris", "Istina",
+                    "Kafka", "Lappland", "Leonhardt", "Liskarm", "Manticore",
+                    "Mayer", "Meteorite", "Mr. Nothing", "Nearl", "Nightmare",
+                    "Platinum", "Pramanix", "Projekt Red", "Provence", "Ptilopsis",
+                    "Reed", "Sesa", "Shamare", "Silence", "Skyfire",
+                    "Sora", "Specter", "Swire", "Texas", "Toddifons",
+                    "Tsukinogi", "Warfarin", "Whisperain", "Zima"]
+
+four_stars_pool = ["Aciddrop", "Ambriel", "April", "Arene", "Beanstalk",
+                "Bubble", "Click", "Coura", "Cutter", "Deepcolor",
+                "Dobermann", "Flint", "Frostleaf", "Gitano", "Gravel",
+                "Greyy", "Gummy", "Haze", "Jackie", "Jaye",
+                "Jessica", "Leizi", "Matoimaru", "Matterhorn", "May",
+                "Meteor", "Mouse", "Myrrh", "Myrtle", "Perfumer",
+                "Pinecone", "Podenco", "Rope", "Scavenger", "Shaw",
+                "Shirayuki", "Sussuro", "Utage", "Vermeil", "Vigna"]
+
+three_stars_pool = ["Adnachiel", "Ansel", "Beagle", "Beehunter", "Cardigan",
+                    "Catapult", "Fang", "Hibiscus", "Kroos", "Lava",
+                    "Melantha", "Midnight", "Orchid", "Plume", "Popukar",
+                    "Spot", "Steward", "Vanilla"]
+
+six_rate_up_pool = ["SilverAsh", "Mountain"]
+five_rate_up_pool = ["Lappland", "Tsukinogi", "Cliffheart"]
