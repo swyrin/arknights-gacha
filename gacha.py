3377acb70060553cf849e5407865fc8920f9575d
Swyrin <dat20036@gmail.com>
2021-10-20 19:23:32 +0700

    Add rate-up stuffs

--- gacha.py
+++ gacha.py
@@ -1,5 +1,7 @@
 import random
 import sys
+from pool import *
+
 
 """
 How this works:
@@ -8,48 +10,84 @@ How this works:
 """
 
 def arknights_gacha(roll_count):
-	rates = [0.02, 0.08, 0.5, 0.4]
-	six_stars_name = '6*'
-	five_stars_name = '5*'
-	four_stars_name = '4*'
-	three_stars_name = '3*'
+    rates = [0.02, 0.08, 0.5, 0.4]
 
-	names = [six_stars_name, five_stars_name, four_stars_name, three_stars_name]
+    # Name
+    six_stars_name = '6*'
+    five_stars_name = '5*'
+    four_stars_name = '4*'
+    three_stars_name = '3*'
 
-	sr_pity_hit = False
-	pity_count = 0
-	choices = random.choices(names, weights=rates, k=roll_count)
+    # Rate-up
+    six_stars_up = 0.5
+    five_stars_up = 0.5
 
-	for idx, choice in enumerate(choices):
-		# guarantee 6*/5* in first ten rolls
-		if idx + 1 <= 10:
-			if (choice == six_stars_name or choice == five_stars_name) and not sr_pity_hit:
-				sr_pity_hit = True
-			if idx + 1 == 10 and not sr_pity_hit:
-				sr_pity_hit = True
-		else:
-			if pity_count >= 50:
-				temp_rates = rates.copy()
-				temp_rates[0] += (pity_count - 50) * 0.02
-				max_index = 0
+    # TODO: Implement debut banner type
+    # four_stars_up = 0.4
 
-				if temp_rates[1] > temp_rates[2] > temp_rates[3]:
-					max_index = 1
-				elif temp_rates[1] < temp_rates[2] and temp_rates[2] > temp_rates[3]:
-					max_index = 2
-				elif temp_rates[1] < temp_rates[2] < temp_rates[3]:
-					max_index = 3
+    names = [six_stars_name, five_stars_name, four_stars_name, three_stars_name]
 
-				temp_rates[max_index] -= 0.02
-				choice = random.choices(names, weights=temp_rates, k=1)[0]
+    sr_pity_hit = False
+    pity_count = 0
+    choices = random.choices(names, weights=rates, k=roll_count)
 
-		if choice == six_stars_name:
-			pity_count = 0
-		else:
-			pity_count += 1
+    for idx, choice in enumerate(choices):
+        temp_rates = rates.copy()
+        # guarantee 6*/5* in first ten rolls
+        if idx + 1 <= 10:
+            if (choice == six_stars_name or choice == five_stars_name) and not sr_pity_hit:
+                sr_pity_hit = True
+            if idx + 1 == 10 and not sr_pity_hit:
+                sr_pity_hit = True
+        else:
+            if pity_count >= 50:
+                rates[0] += (pity_count - 50) * 0.02
+                max_index = 0
 
-		print(f'Roll number {idx + 1}: {choice} \t - Pity count: {pity_count}')
+                if rates[1] > rates[2] > rates[3]:
+                    max_index = 1
+                elif rates[1] < rates[2] and rates[2] > rates[3]:
+                    max_index = 2
+                elif rates[1] < rates[2] < rates[3]:
+                    max_index = 3
+
+                rates[max_index] -= 0.02
+                choice = random.choices(names, weights=rates, k=1)[0]
+
+
+        if choice == six_stars_name:
+            pity_count = 0
+        else:
+            pity_count += 1
+
+        isRateUp = False
+        # Rate-up processing
+        if choice == six_stars_name:
+                up_rate = [six_stars_up, 1 - six_stars_up]
+                up_name = [six_rate_up_pool, list(set(six_stars_pool) - set(six_rate_up_pool))]
+                pick = random.choices(up_name, up_rate, k=1)[0]
+                if pick == six_rate_up_pool:
+                        isRateUp = True
+                choice = random.choice(pick)
+        elif choice == five_stars_name:
+                up_rate = [five_stars_up, 1 - five_stars_up]
+                up_name = [five_rate_up_pool, list(set(five_stars_pool) - set(five_rate_up_pool))]
+                pick = random.choices(up_name, up_rate, k=1)[0]
+                if pick == five_rate_up_pool:
+                        isRateUp = True
+                choice = random.choice(pick)
+        elif choice == four_stars_name:
+                # To-do: Rate-up
+                choice = random.choice(four_stars_pool)
+        elif choice == three_stars_name:
+                choice = random.choice(three_stars_pool)
+
+        if isRateUp:
+                choice += " (RATE UP)"
+            
+        print(f'Roll number {idx + 1}: {choice} \t\t\t\t - Pity count: {pity_count} \t')
+        rates = temp_rates.copy()
 
 if __name__ == "__main__":
-        rolls = int(sys.argv[0])
-        arknights_gacha(rolls)
+        # rolls = int(sys.argv[0])
+        arknights_gacha(300)
