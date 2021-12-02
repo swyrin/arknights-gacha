8968636380e91ef39f5ec9b3fec65b2fcbe35ef8
Tien Dat Pham <dat20036@gmail.com>
2021-12-02 09:40:53 +0700

    Fixed calamities in pool.py

---
+++ core/pool.py
@@ -0,0 +1,33 @@
+six_stars_pool = ["Aak", "Angelina", "Archetto", "Bappipe", "Blaze",
+                  "Blemishine", "Ceobe", "Ch'en", "Eunectes", "Exusiai",
+                  "Eyjafjalla", "Hellagur", "Hoshiguma", "Ifrit", "Kal'tsit",
+                  "Magallan", "Mostima", "Mountain", "Mudrock", "Nightingale",
+                  "Passenger", "Phantom", "Rosa", "Saga", "Saria", "Schwarz",
+                  "Shining", "Siege", "SilverAsh", "Skadi", "Surtr",
+                  "Suzuran", "Thorns", "Weedy"]
+
+five_stars_pool = ["Akafuyu", "Andreana", "Aosta", "April", "Asbestos", "Astesia",
+                   "Ayerscarpe", "Beeswax", "Blue Poison", "Broca", "Chiave",
+                   "Cliffheart", "Croissant", "Elysium", "Executor", "FEater",
+                   "Firewatch", "Flint", "Franka", "Glaucus", "Greythroat",
+                   "Hung", "Iris", "Istina", "Kafka", "Lappland", "Leizi",
+                   "Leonhardt", "Liskarm", "Manticore", "Mayer", "Meteorite",
+                   "Mr. Nothing", "Nearl", "Nightmare", "Platinum", "Pramanix",
+                   "Projekt Red", "Provence", "Ptilopsis", "Reed", "Sesa",
+                   "Shamare", "Silence", "Skyfire", "Sora", "Specter",
+                   "Swire", "Texas", "Toddifons", "Tsukinogi", "Waai Fu",
+                   "Warfarin", "Whisperain", "Zima"]
+
+four_stars_pool = ["Aciddrop", "Ambriel", "Arene", "Beanstalk", "Beehunter"
+                   "Bubble", "Click", "Cuora", "Cutter", "Deepcolor",
+                   "Dobermann", "Earthspirit", "Frostleaf", "Gitano", "Gravel",
+                   "Greyy", "Gummy", "Haze", "Jackie", "Jaye",
+                   "Jessica", "Matoimaru", "Matterhorn", "May", "Meteor",
+                   "Mousse", "Myrrh", "Myrtle", "Perfumer", "Pinecone",
+                   "Podenco", "Rope", "Scavenger", "Shaw", "Shirayuki",
+                   "Sussuro", "Utage", "Vermeil", "Vigna"]
+
+three_stars_pool = ["Adnachiel", "Ansel", "Beagle", "Cardigan",
+                    "Catapult", "Fang", "Hibiscus", "Kroos", "Lava",
+                    "Melantha", "Midnight", "Orchid", "Plume", "Popukar",
+                    "Spot", "Steward", "Vanilla"]
